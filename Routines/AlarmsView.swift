b876733bc78be9d8aa620cf4b84295ed83ab8d77
Ryan Peach <ryan.peach@keysight.com>
2020-04-12 19:01:51 -0400

    Finally Working, first commit.

--- Routines/AlarmsView.swift
+++ Routines/AlarmsView.swift
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AlarmsView: View {
     var body: some View {
         Text("Hello, World!")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct AlarmsView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AlarmsView()
     }
 }
